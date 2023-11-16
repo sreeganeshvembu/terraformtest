b25a9035428ca5efaf2394e715a642bf639e86a6
Sree Ganesh Vembu <vsgv.sreeganesh@gmail.com>
2023-11-16 10:11:44 +0530

    Update outputs.tf

--- outputs.tf
+++ outputs.tf
@@ -1,9 +1,20 @@
-output "resource_group_name" {
-  value       = azurerm_resource_group.example.name
-  description = "The name of the resource group"
+variable "project_name" {
+  default = "Top-105"
 }
 
-output "resource_group_location" {
-  value       = azurerm_resource_group.example.location
-  description = "The location of the resource group"
+variable "environment" {
+  default = "dev"
+}
+
+variable "location" {
+  default = "Germany West Central"  # Adjust as per your Azure region
+}
+
+variable "common_tags" {
+  default = {
+    "project_name"    = "SaVe"
+    "deployment_type" = "IAAC"
+    "deployment_date" = "16-11-2023"
+    "environment"     = "dev"
+  }
 }
