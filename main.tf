c350ee4f6b9f611897e3c9f60881fc20f6f342c7
Sree Ganesh Vembu <vsgv.sreeganesh@gmail.com>
2023-11-16 10:23:38 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -102,5 +102,5 @@ resource "azurerm_subnet" "vnet1_subnet" {
   name                 = "vnet1-subnet"
   resource_group_name  = azurerm_virtual_network.vnet1.resource_group_name
   virtual_network_name = azurerm_virtual_network.vnet1.name
-  address_prefixes       = "10.0.1.0/24"
+  address_prefixes       = ["10.0.1.0/24"]
 }
